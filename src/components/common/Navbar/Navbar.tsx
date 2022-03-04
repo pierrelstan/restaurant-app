7f3acc86cf5c9ebb7d3769812549fa445468683b
Stanley Pierre-Louis <stanleypl75@gmail.com>
2022-03-04 14:17:04 -0400

    feat: add navlist

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -3,24 +3,15 @@ import { AppBar, Toolbar, Button, Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Image from 'next/image';
 import React from 'react';
+import Nav from './Nav';
+import NavList from './NavList';
 import LogoSvg from '../../../../public/static/logo.svg';
-import Link from '../Link';
-
-const Nav = styled('nav')(() => ({}));
 
 const DisplayHeader = styled('div')(({ theme }) => ({
   display: 'none',
   [theme.breakpoints.up('md')]: {
     display: 'block',
   },
-}));
-
-const StyledLink = styled(Link)(() => ({
-  textDecoration: 'none',
-  margin: '5px 15px',
-  flexGrow: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
 }));
 
 const Logo = styled(Box)(({ theme }) => ({
@@ -56,70 +47,11 @@ export default function Navbar() {
             </Logo>
 
             <Nav>
-              <StyledLink
-                variant="button"
-                color="primary.main"
-                href="/"
-                sx={{
-                  fontSize: [4, 16],
-                }}
-              >
-                Home
-              </StyledLink>
-              <StyledLink
-                variant="button"
-                color="primary.main"
-                href="/menu/menu"
-                sx={{
-                  fontSize: [4, 16],
-                }}
-              >
-                Menu
-              </StyledLink>
-              <StyledLink
-                variant="button"
-                color="primary.main"
-                href="/about/about"
-                sx={{
-                  fontSize: [4, 16],
-                }}
-              >
-                About us
-              </StyledLink>
-              <StyledLink
-                variant="button"
-                color="primary.main"
-                href="/story/story"
-                sx={{
-                  fontSize: [4, 16],
-                }}
-              >
-                Our story
-              </StyledLink>
-              <StyledLink
-                variant="button"
-                color="primary.main"
-                href="/blog/blog"
-                sx={{
-                  fontSize: [4, 16],
-                }}
-              >
-                Blog
-              </StyledLink>
-              <StyledLink
-                variant="button"
-                color="primary.main"
-                href="/contact/contact"
-                sx={{
-                  fontSize: [4, 16],
-                }}
-              >
-                Contact
-              </StyledLink>
+              <NavList />
               <Button
                 href="/login/login"
                 variant="outlined"
-                sx={{ my: 1, mx: 1.4, fontSize: [4, 16] }}
+                sx={{ my: 1, mx: 1.4, fontSize: [4, 16], width: 'auto' }}
               >
                 Login
               </Button>
