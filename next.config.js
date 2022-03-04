c5ad557175c747a36428402bf24b3890d2579ec2
Stanley Pierre-Louis <stanleypl75@gmail.com>
2022-03-04 09:17:51 -0400

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,8 @@ const nextConfig = {
   disableStaticImages: false,
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    domains: ['res.cloudinary.com'],
+    // domains: ['res.cloudinary.com'],
+    formats: ['image/png'],
   },
 };
 
