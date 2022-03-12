7d186dd3f1970bfca8f46c006df3fba8133c1c48
Stanley Pierre-Louis <stanleypl75@gmail.com>
2022-03-12 17:47:30 -0400

    style: add isDesktopScreen

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -29,6 +29,13 @@ const StyledLink = styled(Link)(() => ({
   alignItems: 'center',
 }));
 
+const IsDesktopScreen = styled(Box)(({ theme }) => ({
+  display: 'block',
+  [theme.breakpoints.down('md')]: {
+    display: 'none',
+  },
+}));
+
 export default function Footer(): JSX.Element {
   return (
     <Container
@@ -37,110 +44,36 @@ export default function Footer(): JSX.Element {
         mt: '120px',
       }}
     >
-      <Box
-        sx={{
-          textAlign: 'center',
-        }}
-      >
+      <IsDesktopScreen>
         <Box
           sx={{
-            height: 100,
+            textAlign: 'center',
           }}
         >
-          <Logo sx={{ flexGrow: 1 }}>
-            <Image
-              src={LogoSvg as any}
-              alt="yaya-logo"
-              width="70"
-              height="70"
-            />
-          </Logo>
-        </Box>
-        <Nav>
-          <NavList />
-        </Nav>
+          <Box
+            sx={{
+              height: 100,
+            }}
+          >
+            <Logo sx={{ flexGrow: 1 }}>
+              <Image
+                src={LogoSvg as any}
+                alt="yaya-logo"
+                width="70"
+                height="70"
+              />
+            </Logo>
+          </Box>
+          <Nav>
+            <NavList />
+          </Nav>
 
-        <Box
-          sx={{
-            mt: 4,
-          }}
-        >
-          <Box>
-            <Typography
-              variant="h6"
-              sx={{
-                fontSize: '16px',
-                color: 'primary.main',
-                p: 1,
-              }}
-            >
-              © 2021 Finsweet | All rights reserved.
-            </Typography>
-          </Box>
-          <Box>
-            <StyledLink
-              variant="button"
-              color="primary.main"
-              href="https://www.facebook.com"
-              passHref={true}
-              target="_blank"
-            >
-              <Image
-                width={15}
-                height={15}
-                src={Facebook}
-                alt={'facebook-logo'}
-              />
-            </StyledLink>
-            <StyledLink
-              variant="button"
-              color="primary.main"
-              href="https://www.twitter.com"
-              passHref={true}
-              target="_blank"
-            >
-              <Image
-                width={15}
-                height={15}
-                src={Twitter}
-                alt={'facebook-logo'}
-              />
-            </StyledLink>
-            <StyledLink
-              variant="button"
-              color="primary.main"
-              href="https://www.instagram.com"
-              passHref={true}
-              target="_blank"
-            >
-              <Image
-                width={15}
-                height={15}
-                src={Instagram}
-                alt={'facebook-logo'}
-              />
-            </StyledLink>
-            <StyledLink
-              variant="button"
-              color="primary.main"
-              href="https://www.linkiden.com"
-              passHref={true}
-              target="_blank"
-            >
-              <Image
-                width={15}
-                height={15}
-                src={Linkedin}
-                alt={'facebook-logo'}
-              />
-            </StyledLink>
-          </Box>
-          <Box>
-            <StyledLink
-              variant="button"
-              color="primary.main"
-              href="/contact/contact"
-            >
+          <Box
+            sx={{
+              mt: 4,
+            }}
+          >
+            <Box>
               <Typography
                 variant="h6"
                 sx={{
@@ -149,12 +82,88 @@ export default function Footer(): JSX.Element {
                   p: 1,
                 }}
               >
-                Contact Us
+                © 2021 Finsweet | All rights reserved.
               </Typography>
-            </StyledLink>
+            </Box>
+            <Box>
+              <StyledLink
+                variant="button"
+                color="primary.main"
+                href="https://www.facebook.com"
+                passHref={true}
+                target="_blank"
+              >
+                <Image
+                  width={15}
+                  height={15}
+                  src={Facebook}
+                  alt={'facebook-logo'}
+                />
+              </StyledLink>
+              <StyledLink
+                variant="button"
+                color="primary.main"
+                href="https://www.twitter.com"
+                passHref={true}
+                target="_blank"
+              >
+                <Image
+                  width={15}
+                  height={15}
+                  src={Twitter}
+                  alt={'facebook-logo'}
+                />
+              </StyledLink>
+              <StyledLink
+                variant="button"
+                color="primary.main"
+                href="https://www.instagram.com"
+                passHref={true}
+                target="_blank"
+              >
+                <Image
+                  width={15}
+                  height={15}
+                  src={Instagram}
+                  alt={'facebook-logo'}
+                />
+              </StyledLink>
+              <StyledLink
+                variant="button"
+                color="primary.main"
+                href="https://www.linkiden.com"
+                passHref={true}
+                target="_blank"
+              >
+                <Image
+                  width={15}
+                  height={15}
+                  src={Linkedin}
+                  alt={'facebook-logo'}
+                />
+              </StyledLink>
+            </Box>
+            <Box>
+              <StyledLink
+                variant="button"
+                color="primary.main"
+                href="/contact/contact"
+              >
+                <Typography
+                  variant="h6"
+                  sx={{
+                    fontSize: '16px',
+                    color: 'primary.main',
+                    p: 1,
+                  }}
+                >
+                  Contact Us
+                </Typography>
+              </StyledLink>
+            </Box>
           </Box>
         </Box>
-      </Box>
+      </IsDesktopScreen>
     </Container>
   );
 }
