dc92f4b7d97e9fc8bc895b08c56c12f0f6752b68
Stanley Pierre-Louis <stanleypl75@gmail.com>
2022-03-17 13:44:23 -0400

    fix: remove logo and nav in the footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -6,20 +6,8 @@ import React from 'react';
 import Facebook from '../../../../public/static/facebook.png';
 import Instagram from '../../../../public/static/instagram.png';
 import Linkedin from '../../../../public/static/linkedin.png';
-import LogoSvg from '../../../../public/static/logo.svg';
 import Twitter from '../../../../public/static/twitter.png';
 import Link from '../Link';
-import Nav from '../Navbar/Nav';
-import NavList from '../Navbar/NavList';
-
-const Logo = styled(Box)(({ theme }) => ({
-  flexGrow: 1,
-  [theme.breakpoints.down('md')]: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-}));
 
 const StyledLink = styled(Link)(() => ({
   textDecoration: 'none',
@@ -29,141 +17,78 @@ const StyledLink = styled(Link)(() => ({
   alignItems: 'center',
 }));
 
-const IsDesktopScreen = styled(Box)(({ theme }) => ({
-  display: 'block',
-  [theme.breakpoints.down('md')]: {
-    display: 'none',
-  },
-}));
-
 export default function Footer(): JSX.Element {
   return (
     <Container
       maxWidth="xl"
       sx={{
         mt: '120px',
+        textAlign: 'center',
       }}
     >
-      <IsDesktopScreen>
-        <Box
+      <Box>
+        <Typography
+          variant="h6"
           sx={{
-            textAlign: 'center',
+            fontSize: '16px',
+            color: 'primary.main',
+            p: 1,
           }}
         >
-          <Box
-            sx={{
-              height: 100,
-            }}
-          >
-            <Logo sx={{ flexGrow: 1 }}>
-              <Image
-                src={LogoSvg as any}
-                alt="yaya-logo"
-                width="70"
-                height="70"
-              />
-            </Logo>
-          </Box>
-          <Nav>
-            <NavList />
-          </Nav>
+          Follow us on:
+        </Typography>
+      </Box>
+      <Box>
+        <StyledLink
+          variant="button"
+          color="primary.main"
+          href="https://www.facebook.com"
+          passHref={true}
+          target="_blank"
+        >
+          <Image width={15} height={15} src={Facebook} alt={'facebook-logo'} />
+        </StyledLink>
+        <StyledLink
+          variant="button"
+          color="primary.main"
+          href="https://www.twitter.com"
+          passHref={true}
+          target="_blank"
+        >
+          <Image width={15} height={15} src={Twitter} alt={'facebook-logo'} />
+        </StyledLink>
+        <StyledLink
+          variant="button"
+          color="primary.main"
+          href="https://www.instagram.com"
+          passHref={true}
+          target="_blank"
+        >
+          <Image width={15} height={15} src={Instagram} alt={'facebook-logo'} />
+        </StyledLink>
+        <StyledLink
+          variant="button"
+          color="primary.main"
+          href="https://www.linkiden.com"
+          passHref={true}
+          target="_blank"
+        >
+          <Image width={15} height={15} src={Linkedin} alt={'facebook-logo'} />
+        </StyledLink>
+      </Box>
 
-          <Box
-            sx={{
-              mt: 4,
-            }}
-          >
-            <Box>
-              <Typography
-                variant="h6"
-                sx={{
-                  fontSize: '16px',
-                  color: 'primary.main',
-                  p: 1,
-                }}
-              >
-                © 2021 Finsweet | All rights reserved.
-              </Typography>
-            </Box>
-            <Box>
-              <StyledLink
-                variant="button"
-                color="primary.main"
-                href="https://www.facebook.com"
-                passHref={true}
-                target="_blank"
-              >
-                <Image
-                  width={15}
-                  height={15}
-                  src={Facebook}
-                  alt={'facebook-logo'}
-                />
-              </StyledLink>
-              <StyledLink
-                variant="button"
-                color="primary.main"
-                href="https://www.twitter.com"
-                passHref={true}
-                target="_blank"
-              >
-                <Image
-                  width={15}
-                  height={15}
-                  src={Twitter}
-                  alt={'facebook-logo'}
-                />
-              </StyledLink>
-              <StyledLink
-                variant="button"
-                color="primary.main"
-                href="https://www.instagram.com"
-                passHref={true}
-                target="_blank"
-              >
-                <Image
-                  width={15}
-                  height={15}
-                  src={Instagram}
-                  alt={'facebook-logo'}
-                />
-              </StyledLink>
-              <StyledLink
-                variant="button"
-                color="primary.main"
-                href="https://www.linkiden.com"
-                passHref={true}
-                target="_blank"
-              >
-                <Image
-                  width={15}
-                  height={15}
-                  src={Linkedin}
-                  alt={'facebook-logo'}
-                />
-              </StyledLink>
-            </Box>
-            <Box>
-              <StyledLink
-                variant="button"
-                color="primary.main"
-                href="/contact/contact"
-              >
-                <Typography
-                  variant="h6"
-                  sx={{
-                    fontSize: '16px',
-                    color: 'primary.main',
-                    p: 1,
-                  }}
-                >
-                  Contact Us
-                </Typography>
-              </StyledLink>
-            </Box>
-          </Box>
-        </Box>
-      </IsDesktopScreen>
+      <Box>
+        <Typography
+          variant="h6"
+          sx={{
+            fontSize: '16px',
+            color: 'primary.main',
+            p: 1,
+          }}
+        >
+          © 2021 Finsweet | All rights reserved.
+        </Typography>
+      </Box>
     </Container>
   );
 }
